8948f572478fbd5ea9db8b12de93309d635bec99
mrcirniko <86667038+mrcirniko@users.noreply.github.com>
2022-12-06 18:24:07 +0300

    Update lab14.c

--- LAB14/lab14.c
+++ LAB14/lab14.c
@@ -21,15 +21,16 @@ void matInput(int n, int (*mat)[n]){
 void matSolve(int n, int (*mat)[n]){
     int k = n - 1;
     for (int i = 0; i <= n / 2 - 1; ++i) {
-        for (int j = i; j <= k - 1; j++)
+        for (int j = i; j <= k - 1; ++j)
             printf("%d ", mat[j][i]);
-        for (int j = i; j <= k - 1; j++)
+        for (int j = i; j <= k - 1; ++j)
             printf("%d ", mat[k][j]);
-        for (int j = k; j >= i + 1; j = j - 1)
+        for (int j = k; j >= i + 1; j -= 1)
             printf("%d ", mat[j][k]);
-        for (int j = k; j >= i + 1; j = j - 1)
+        for (int j = k; j >= i + 1; j -= 1)
             printf("%d ", mat[i][j]);
-        k = k - 1;
+        k -= 1;
     }
-    if (n % 2 != 0) printf("%d\n", mat[n / 2][n / 2]);
+    if (n % 2 != 0)
+        printf("%d\n", mat[n / 2][n / 2]);
 }
