39aaf01af4094afa9709694a082f3494be727a57
mrcirniko <86667038+mrcirniko@users.noreply.github.com>
2022-12-06 15:26:19 +0300

    Create lab11.c

---
+++ LAB11/lab11.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+bool isOct(char x) {
+    if ((x - '0') >= 0 && (x - '0') <= 7) return true;
+    return false;
+}
+typedef enum {
+    OUT,//слово, которое нужно пропустить
+    IN_DIGIT,//нужное слово
+    SPACE//пробел или переход строки
+} state;
+
+int number_of_octal(){
+    state state = OUT;
+    int res = 0;
+    int num = 0;
+    int space = 0;
+    int sign = 1;
+    int i = 0;
+    for (char a = getchar(); a != EOF; a = getchar()) {
+        if (i == 0) i = 1; else i = 2;
+        switch (state) {
+            case OUT:
+                if (isOct(a)) state = IN_DIGIT;
+                else if (a == ' ' || a == '\n') {
+                    state = SPACE;
+                } else {
+                    if (a == '-' && space == 1) sign = -1;
+                    num = 0;
+                    state = OUT;
+                    break;
+                }
+            case IN_DIGIT:
+                if (isOct(a)) {
+                    if (space == 1 || i == 1) {
+                        if (a == '0') num = num*8; else
+                            num = num*8 + a - '0';
+                        space = 0;
+                        state = OUT;
+                        break;
+                    } else if (space == 0 && num != 0) {
+                        if (a == '0') num = num*8; else
+                            num = num*8 + a - '0';
+                        state = OUT;
+                        break;
+                    } else if (space == 0 && num == 0) {
+                        num = 0;
+                        state = OUT;
+                        break;
+                    }
+                }
+            case SPACE:
+                if (num*sign < 1000 && num*sign > 10) {
+                    res++;
+                }
+                sign = 1;
+                space = 1;
+                num = 0;
+                state = OUT;
+                break;
+        }
+    }
+    if (num*sign < 1000 && num*sign > 10) {
+        res++;
+    }
+    return res;
+}
