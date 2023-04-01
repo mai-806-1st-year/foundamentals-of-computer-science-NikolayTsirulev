7846158051358727a0b64e497064d6b310f26df8
mrcirniko <86667038+mrcirniko@users.noreply.github.com>
2023-04-01 16:25:21 +0300

    Update l21.sh

--- LAB21/l21.sh
+++ LAB21/l21.sh
@@ -6,3 +6,4 @@ output_file=$3
 max_size=$4
 
 find "$directory" -type f -name "*.$suffix" -size "-${max_size}c" -printf "%f %s\n" > "$output_file"
+find "$directory" -type f -name "*.$suffix" -size "-${max_size}c" -perm /u=x,g=x,o=x -printf "%f %s\n" > "$output_file"
