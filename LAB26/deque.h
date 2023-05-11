22b30cde79146af52b1ecd0bb0c8d863cee58ac5
mrcirniko <86667038+mrcirniko@users.noreply.github.com>
2023-05-11 21:04:36 +0300

    Update deque.h

--- LAB26/deque.h
+++ LAB26/deque.h
@@ -3,7 +3,6 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stddef.h>
 
 #include "types.h"
@@ -17,15 +16,17 @@ typedef struct
     int capacity;
     int size;
     T data[];
-} Deque;
+} deq;
 
-Deque *create_deq(int size);               //создать дек
-Deque *push_back(Deque *d, T elem);        //вставить в конец
-T pop_back(Deque *d);                      //удалить из конца
-Deque *push_front(Deque *d, T elem);       //вставить в начало
-T pop_front(Deque *d);                     //удалить из начала
-bool is_empty(Deque *d);                   //true, если дек пустой
-bool is_full(Deque *d);                    //true, если дек полный
-void deque_destroy(Deque*);                //удалить дек
+deq *create_deq(int size);
+deq *push_back(deq *d, T elem);
+T pop_back(deq *d);
+T top_back(deq *d);
+deq *push_front(deq *d, T elem);
+T pop_front(deq *d);
+T top_front(deq *d);
+bool is_empty(deq *d);
+bool is_full(deq *d);
+void print_deq(deq *d);
 
 #endif //_DEQUE_H_
