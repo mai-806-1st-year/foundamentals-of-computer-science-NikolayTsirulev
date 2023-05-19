3e67619f7090bb74ab68d83ff866ef5762a2d99e
mrcirniko <86667038+mrcirniko@users.noreply.github.com>
2023-05-20 01:03:44 +0300

    Create main.c

---
+++ LAB26/main.c
@@ -0,0 +1,46 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "deque.h"
+
+void test1()
+{
+    deq *d = create_deq(4);
+    while (!is_full(d))
+    {
+        T el = rand() % 10;
+        if (el % 2) {
+            printf("push front: %d\n", el);
+            push_front(d, el);
+        } else {
+            printf("push back: %d\n", el);
+            push_back(d, el);
+        }
+    }
+    iterator it = get_iterator(d, true);
+    printf("print deque from head to tail:\n");
+    while (it.index != -1) {
+        printf("%d ", next(&it));
+    }
+    it = get_iterator(d, false);
+    printf("\nprint deque from tail to head:\n");
+    while (it.index != -1) {
+        printf("%d ", prev(&it));
+    }
+    printf("\n");
+    d = sort(d);
+    it = get_iterator(d, true);
+    printf("\nprint deque after merge sort:\n");
+    while (it.index != -1) {
+        printf("%d ", next(&it));
+    }
+    printf("\n");
+    printf("head: %d\n", top_front(d));
+    printf("tail: %d\n", top_back(d));
+    delete(d);
+}
+
+int main(void)
+{
+    test1();
+}
