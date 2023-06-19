2f023b996258805e8c42f7821685c813c00a7187
mrcirniko <86667038+mrcirniko@users.noreply.github.com>
2023-06-19 22:30:24 +0300

    Update persons_dump.c

--- CP6/persons_dump.c
+++ CP6/persons_dump.c
@@ -1,28 +1,30 @@
 #include "person.h"
+#include <stdbool.h>
 
-int main(int argc, char* argv[]){
-    if (argc != 3){
+bool ReadData (person* p, FILE* in) {
+    return fscanf(in, "%[^ ] %[^ ] %c %d %c %u %u %u %c\n", p->surname, p->initials, &p->gender, &p->school_number, &p->medal, &p->math, &p->physics, &p->computer_science, &p->essay) == 9;
+    }
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
         fprintf(stderr, "Wrong number of args!\n");
         return 2;
     }
     FILE* in = fopen(argv[1], "r");
-    FILE* out = fopen(argv[2], "wb");
-    if (in == NULL){
+    FILE* out = fopen(argv[2], "w");
+    if (in == NULL) {
         fprintf(stderr, "Can't open in file!\n");
         return 1;
     }
-    if (out == NULL){
+    if (out == NULL) {
         fprintf(stderr, "Can't create out file!\n");
         return 3;
     }
     person p;
     int c = 0;
     fseek(out, sizeof(int), SEEK_SET);
-    while(fscanf(in, "%[^ ] %[^ ] %c %d %c %u %u %u %c\n", p.surname, p.initials, &p.gender, &p.school_number, &p.medal, &p.math, &p.physics, &p.computer_science, &p.essay) == 9){
-        if (fwrite(&p, sizeof(p), 1, out) != 1){
-            fprintf(stderr, "Write error!\n");
-            return 1;
-        }
+    while(ReadData(&p, in)) {
+    	fwrite(&p, sizeof(person), 1, out);
         c++;
     }
     fseek(out, 0, SEEK_SET);
@@ -30,7 +32,7 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "Write error!\n");
         return 1;
     }
-	printf("%d lines are written\n", c);
+    printf("%d lines are written\n", c);
     fclose(in);
     fclose(out);
     return 0;
