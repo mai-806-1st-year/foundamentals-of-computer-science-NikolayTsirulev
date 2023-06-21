e4122606b66cc65e059f59ce0c127db985552c7a
mrcirniko <86667038+mrcirniko@users.noreply.github.com>
2023-06-22 01:00:40 +0300

    Update table.h

--- CP9/table.h
+++ CP9/table.h
@@ -5,13 +5,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "complex.h"
 
-int table_read(data_str*, FILE*);
-void table_print(data_str*, size_t size);
-void bin_ins_sort(data_str*, size_t size);
-void binary_search(data_str*, size_t size, key_complex);
-void table_reverse(data_str*, size_t size);
-void table_rand(data_str*, size_t size);
+int table_read(data_str table[], FILE* in);
+void table_print(data_str table[], size_t size);
+void bin_ins_sort(data_str table[], size_t size);
+void binary_search(data_str table[], size_t size, key_complex k);
+void table_reverse(data_str table[], size_t size);
+void table_rand(data_str table[], size_t size);
 
 #endif
