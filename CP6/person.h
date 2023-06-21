73486d01e07b992499f8b4eef362e6dcb9c28513
mrcirniko <86667038+mrcirniko@users.noreply.github.com>
2023-06-22 00:47:29 +0300

    Update person.h

--- CP6/person.h
+++ CP6/person.h
@@ -2,19 +2,20 @@
 #define __person_h__
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
 typedef unsigned int uint;
 
-typedef struct{
+typedef struct {
     char surname[50];
     char initials[2];
-    char gender;
+    bool gender;
     uint school_number;
-    char medal;
+    bool medal;
     uint math;
     uint physics;
     uint computer_science;
-    char essay;
+    bool essay;
 } person;
 #endif
