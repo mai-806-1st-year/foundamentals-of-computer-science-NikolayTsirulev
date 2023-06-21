a4440b30825d8c0a2e2e84c6c91a10f633576263
mrcirniko <86667038+mrcirniko@users.noreply.github.com>
2023-06-22 00:58:40 +0300

    Update complex.h

--- CP9/complex.h
+++ CP9/complex.h
@@ -6,26 +6,26 @@
 
 #define MAX_STRING_SIZE 100
 
-typedef struct{
+typedef struct {
     double real;
     double img;
 } key_complex;
 
-typedef struct{
+typedef struct {
     key_complex key;
     char str[MAX_STRING_SIZE];
 } data_str;
 
-bool complex_read(key_complex*);
+bool complex_read(key_complex* x);
 
-bool complex_file_read(key_complex*, FILE*);
+bool complex_file_read(key_complex* x, FILE* in);
 
-bool complex_is_equal(key_complex, key_complex);
+bool complex_is_equal(key_complex x1, key_complex x2);
 
 int fcompare(double a, double b);
 
 bool key_compare(key_complex a, key_complex b);
 
-void complex_print(key_complex);
+void complex_print(key_complex x);
 
 #endif
