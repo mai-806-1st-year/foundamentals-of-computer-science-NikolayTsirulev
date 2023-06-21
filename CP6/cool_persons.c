877080affe4623df8b60d3454244689a142588b7
mrcirniko <86667038+mrcirniko@users.noreply.github.com>
2023-06-22 00:46:06 +0300

    Update cool_persons.c

--- CP6/cool_persons.c
+++ CP6/cool_persons.c
@@ -4,35 +4,11 @@ void usage() {
     printf("Usage: program filename\n");
 }
 
-void PrintCoolPersons(FILE* file, int c) {
-    person p[c];
-    uint marks[c][4];
-    for (int i = 0; i < c; ++i) {
-        fread(&p[i], sizeof(person), 1, file);
-        marks[i][0] = 0;
-        marks[i][1] = 0;
-        marks[i][2] = 0;
-        marks[i][3] = 0;
-        if((p[i].gender == 'W')) {
-            marks[i][0] = p[i].math;
-            marks[i][1] = p[i].physics;
-            marks[i][2] = p[i].computer_science;
-        }
-    }
-    for (int i = 0; i < c; ++i) {
-        int k = 0;
-        for (int j = 0; j < c; ++j) {
-            if (marks[i][0]!=marks[j][0]&&marks[i][1]!=marks[j][1]&&marks[i][2]!=marks[j][2]&&i!=j) {
-                k++;
-            }
-        }
-        if (k==c-1) {
-            marks[i][3] = 1;
-        }
-    }
-    for (int i = 0; i < c; ++i) {
-        if(marks[i][3]){
-            printf("%s math: %u physics: %u computer science: %u\n", p[i].surname, p[i].math, p[i].physics, p[i].computer_science);
+void PrintCoolPersons(FILE* file) {
+    person p;
+    while (fread (&p, sizeof(person), 1, file) == 1) {
+        if(!p.gender&&p.math!=p.physics&&p.math!=p.computer_science&&p.physics!=p.computer_science){
+            printf("%s math: %u physics: %u computer science: %u\n", p.surname, p.math, p.physics, p.computer_science);
         }
     }
 }
@@ -48,11 +24,6 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Can't open file!\n");
         return 1;
     }
-    int c;
-    if (fread(&c, sizeof(int), 1, file) != 1) {
-        fprintf(stderr, "Read number of elems error!\n");
-        return 1;
-    }
-    PrintCoolPersons(file, c);
+    PrintCoolPersons(file);
     return 0;
 }
